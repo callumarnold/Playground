13c5c7f10c5f9b4e726de9f170d40cdd032eb952
Callum Edward Arnold <callumarnold@hotmail.com>
2021-02-26 15:34:28 +0000

    Added new script that counts vowels in input string

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Your code goes here');
+console.log("The word 'games' has " + countTheVowels("games").toString() + " vowels.");
+console.log("The word 'video' has " + countTheVowels("video").toString() + " vowels.");
+function countTheVowels(input) {
+    //let wordArray: string[] = Object.assign([], input);
+    var vowelCount = 0;
+    for (var i = 0; i < input.length; i++) {
+        var letter = input.charAt(i);
+        if (isVowel(letter)) {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
+function isVowel(letter) {
+    var upper = letter.toUpperCase();
+    if ((upper == 'A') || (upper == 'I') || (upper == 'E') || (upper == 'O') || (upper == 'U')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,32 @@
-console.log('Your code goes here');
+console.log("The word 'games' has " + countTheVowels("games").toString() + " vowels.");
+console.log("The word 'video' has " + countTheVowels("video").toString() + " vowels.");
+
+function countTheVowels(input: string)
+{
+    //let wordArray: string[] = Object.assign([], input);
+    let vowelCount: number = 0;
+
+    for(let i = 0; i < input.length; i++)
+    {
+        let letter: string = input.charAt(i);
+        if (isVowel(letter))
+        {
+            vowelCount++;
+        }
+    }
+
+    return vowelCount;
+}
+
+function isVowel(letter: string)
+{
+    let upper: string = letter.toUpperCase();
+    if((upper == 'A') || (upper == 'I') || (upper == 'E') || (upper == 'O') || (upper == 'U'))
+    {
+        return true;       
+    }
+    else
+    {
+        return false;
+    }
+}
